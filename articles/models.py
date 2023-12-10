8fad8bf631b122e777615607b8e4e077fe0a8a62
abel malu <abellmalu@gmail.com>
2023-12-10 09:04:07 +0300

    article list page is done

---
+++ articles/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class  Article(models.Model):
+    title=models.CharField(max_length=255)
+    slug=models.SlugField()
+    body=models.TextField()
+    date=models.TimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+    def snippet(self):
+        return self.body[:50]+'........'
+
+
+
+
+
+
+
--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -1,7 +1,15 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    path('', views.homepage),
     path('admin/', admin.site.urls),
+    path('about/', views.about),
+    path('article/', include('articles.urls'))
+    
 ]
+
+urlpatterns += staticfiles_urlpatterns()
