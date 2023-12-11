33b1180699758a5680c6dcd7e527a9a61f256723
abel malu <abellmalu@gmail.com>
2023-12-11 21:43:49 +0300

    article creation form created

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -7,8 +7,9 @@ class  Article(models.Model):
     title=models.CharField(max_length=255)
     slug=models.SlugField()
     body=models.TextField()
-    date=models.TimeField(auto_now_add=True)
+    date=models.DateTimeField(auto_now_add=True)
     thumb=models.ImageField(default='default.png',blank=True)
+    author=models.ForeignKey(User,on_delete=models.CASCADE, null=True)
 
     def __str__(self) -> str:
         return self.title
--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .forms import CreateArticle
+
+
+
 # Create your views here.
 def artcile_list(request):
     articles = Article.objects.all().order_by('date')
@@ -11,7 +15,19 @@ def article_detail(request,slug):
     article=Article.objects.get(slug=slug)
     return render(request,'articles/article_detail.html', {'article':article})
 
-@login_required(login_url='/accounts/login/')
+@login_required()
 def artcile_create(request):
+    if request.method=='POST':
+        form = CreateArticle(request.POST,request.FILES)
+        if form.is_valid():
+            
+            instance=form.save(commit=False)
+            instance.author=request.user
+            instance.save()
+            #save article 
+            return redirect('articles:list')
 
-    return render(request,'articles/article_create.html')
+    else:   
+        form = CreateArticle()
+     
+    return render(request,'articles/article_create.html',{'form':form})
